9b3299c6370987a279d8c8092074d90451b265bb
fat83dotcom <fernando.quimic@gmail.com>
2024-07-12 08:11:27 -0300

    view para a pagina de recitas - implementação de titulo para as abas de navegador.

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -3,7 +3,22 @@ from django.views import View
 
 
 class Home(View):
-    template = 'recipes/home.html'
+    template = 'recipes/home/home.html'
+    title = 'Home'
 
     def get(self, request):
-        return render(request, self.template)
+        context = {
+            'title': self.title
+        }
+        return render(request, self.template, context)
+
+
+class RecipeView(View):
+    template = 'recipes/recipe_view/recipe_view.html'
+    title = 'Receita'
+
+    def get(self, request, cod):
+        context = {
+            'title': self.title
+        }
+        return render(request, self.template, context)
