69fa8a820d485971010bbd1bfbc7d62cca031586
fat83dotcom <fernando.quimic@gmail.com>
2024-07-16 09:03:28 -0300

    Models para a receitas e a categoria.

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=50)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=50)
+    preparations_steps = models.TextField()
+    preparations_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/cover/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
