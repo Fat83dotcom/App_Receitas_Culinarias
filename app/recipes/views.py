7ac073046747c7819b8d9a92b2b4a2896692764a
fat83dotcom <fernando.quimic@gmail.com>
2024-07-18 13:07:32 -0300

    Implementando o model das categorias.

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -22,9 +22,25 @@ class RecipeView(View):
     title = 'Receita'
 
     def get(self, request, cod):
+        recipe = Recipe.objects.filter(id=cod)
         context = {
             'title': self.title,
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail': True,
+        }
+        return render(request, self.template, context)
+
+
+class CategoryView(View):
+    template = 'recipes/home/home.html'
+    title = 'Categorias'
+
+    def get(self, request, cod_category):
+        recipes = Recipe.objects.filter(
+            category__id=cod_category
+        ).order_by('-id')
+        context = {
+            'title': self.title,
+            'recipes': recipes,
         }
         return render(request, self.template, context)
