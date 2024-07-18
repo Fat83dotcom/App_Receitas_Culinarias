9ab5b5396608abdc46e4ed6dea231d0f23ccfa2c
fat83dotcom <fernando.quimic@gmail.com>
2024-07-18 13:04:59 -0300

    Implementando a área admin para os models.

--- app/recipes/admin.py
+++ app/recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title']
