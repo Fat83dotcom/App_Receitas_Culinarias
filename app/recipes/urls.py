d9c084a243c5c656ce6e9c3ecc90ce72618e4778
fat83dotcom <fernando.quimic@gmail.com>
2024-07-18 13:05:43 -0300

    Urls para as categorias.O

--- app/recipes/urls.py
+++ app/recipes/urls.py
@@ -4,4 +4,9 @@ from recipes import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('recipes/<int:cod>/', views.RecipeView.as_view(), name='recipe_view'),
+    path(
+        'categories/<int:cod_category>/',
+        views.CategoryView.as_view(),
+        name='category'
+    ),
 ]
