6236e5c0214a2085b658e96d65f355718a814d2f
fat83dotcom <fernando.quimic@gmail.com>
2024-11-01 11:48:25 -0300

    Testes para as urls,

--- app/recipes/tests.py
+++ app/recipes/tests.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipesURLsTest(TestCase):
-    def test_ok(self):
-        ...
+    def test_home_url_is_correct(self):
+        url = reverse('home')
+        print(f'Caminho retornado por reverse: {url}')
+        self.assertEqual(url, '/')
+
+    def test_recipes_url_is_correct(self):
+        url = reverse('recipe_view', args=(1,))
+        print(f'Caminho retornado por reverse: {url}')
+        self.assertEqual(url, '/recipes/1/')
+
+    def test_category_url_is_correct(self):
+        url = reverse('category', args=(100,))
+        print(f'Caminho retornado por reverse: {url}')
+        self.assertEqual(url, '/categories/100/')
