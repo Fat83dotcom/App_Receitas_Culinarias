d787b9b2fa8dff7bd8d70829221d4d563e6a183f
fat83dotcom <fernando.quimic@gmail.com>
2024-11-12 12:08:00 -0300

    Modulo para testes de reuisições e respostas http.

---
+++ app/recipes/tests/test_recipes_request_response_status.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+import pdb
+
+
+class RecipeRequestResponse(TestCase):
+    def test_recipes_request_response_200_OK(self):
+        response = self.client.get(reverse('home'))
+        # pdb.set_trace()
+        print(response)
