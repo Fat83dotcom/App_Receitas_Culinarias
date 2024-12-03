2eda866495cccf7577a47e1da413af593ff26a3d
fat83dotcom <fernando.quimic@gmail.com>
2024-12-03 12:04:38 -0300

    Novos testes para os templates.

--- app/recipes/tests/test_recipes_templates.py
+++ app/recipes/tests/test_recipes_templates.py
@@ -7,3 +7,15 @@ class TemplatesLoadTest(TestCase):
     def test_recipe_home_view_load_correct_template(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'recipes/home/home.html')
+
+    def test_recipe_ecipe_view_view_load_correct_template(self):
+        response = self.client.get(reverse('recipe_view', args=(1,)))
+        self.assertTemplateUsed(
+            response, 'recipes/recipe_view/recipe_view.html'
+        )
+
+    def test_recipe_category_view_load_correct_template(self):
+        response = self.client.get(reverse('category', args=(30,)))
+        self.assertTemplateUsed(
+            response, 'recipes/category/category.html'
+        )
